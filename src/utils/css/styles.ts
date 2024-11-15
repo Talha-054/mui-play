e109714f03459fe5d1f39559f033311b3ac8f53e
Muhammad Talha <03345238002h@gmail.com>
2024-11-15 11:21:14 +0500

    build error fix

--- src/utils/css/styles.ts
+++ src/utils/css/styles.ts
@@ -1,2 +1,9 @@
-const cssStyles  = Object.keys(document.getElementsByTagName('HTML')[0].style) 
+const cssStyles  = Object.keys(document.getElementsByTagName('html')[0].style) 
+console.log(Object.keys(document.getElementsByTagName("html")[0]));
+console.log(cssStyles);
+
+
 export const  allCssStyles = ["mr","ml","mb","mt","pl","pr","pb","pt","paddingX","paddingY","px","py","my","mx","marginY","marginX"].concat(cssStyles)
+
+
+
